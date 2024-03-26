cfaafefa5f6ccabe718d00a96ef9e20cd57ed24d
Anaïs <157112079+anaislhostis@users.noreply.github.com>
2024-03-26 13:32:30 +0100

    Add files via upload

---
+++ main/java/com/example/e_carterose/AnimauxEnTransfertFragment.java
@@ -0,0 +1,121 @@
+package com.example.e_carterose;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.text.Spanned;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.core.text.HtmlCompat;
+import androidx.fragment.app.Fragment;
+
+import com.example.e_carterose.databinding.FragmentAnimauxEnTransfertBinding;
+
+import java.util.List;
+
+
+public class AnimauxEnTransfertFragment extends Fragment {
+
+    private DatabaseAccess db;
+    private List<Animal> ElevageTransfer; // Variable pour stocker la liste des animaux en transfert entre élevage
+    private List<Animal> SlaughterhouseTransfer; // Variable pour stocker la liste des animaux en transfert vers l'abattoir
+    private LinearLayout layout_arrivals;
+    private LinearLayout layout_departures;
+    private LinearLayout layout_slaughterhouse;
+    private FragmentAnimauxEnTransfertBinding binding;
+
+    public static AnimauxEnTransfertFragment newInstance() {
+        AnimauxEnTransfertFragment fragment = new AnimauxEnTransfertFragment();
+        return fragment;
+    }
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_animaux_en_transfert, container, false); // Initialiser la vue
+        layout_arrivals = rootView.findViewById(R.id.arrivals_animals_container); // Récupérer le layout pour afficher les animaux qui arrivent dans un autre élevage
+        layout_departures = rootView.findViewById(R.id.departures_animals_container); // Récupérer le layout pour afficher les animaux qui partent dans un autre élevage
+        layout_slaughterhouse = rootView.findViewById(R.id.slaughterhouse_animals_container); // Récupérer le layout pour afficher les animaux qui vont à l'abattoir
+        binding = FragmentAnimauxEnTransfertBinding.inflate(inflater, container, false); // Initialiser la liaison de données
+
+        db = new DatabaseAccess(requireContext()); // Initialiser la base de données
+
+        // Récupérer le numéro de l'élevage depuis la variable statique
+        String numElevage = MainActivity.numeroElevage;
+        Log.e("numElevage", numElevage);
+
+
+        // Supprimer tous les éléments actuellement affichés
+        layout_arrivals.removeAllViews();
+        layout_departures.removeAllViews();
+        layout_slaughterhouse.removeAllViews();
+
+
+        ElevageTransfer = db.getAnimalsTransferELevageByElevage(numElevage);
+        SlaughterhouseTransfer = db.getAnimalsTransferslaughterhouseByElevage(numElevage);
+        Log.e("Animaux transferés entre élevages : ", ElevageTransfer.toString());
+        Log.e("Animaux transferés abattoir : ", SlaughterhouseTransfer.toString());
+
+        // Parcourir la liste des animaux de l'élevage
+        for (Animal animal : ElevageTransfer) {
+
+            String numNat = animal.getNumNat();
+            Log.e("numNat", numNat); // valeur null
+            if (numNat != null && numNat.startsWith("000000")) {
+
+                // Ajouter l'animal au layout des départs
+                addAnimalToLayout(animal, layout_departures);
+            } else {
+                // Ajouter l'animal au layout des arrivées
+                addAnimalToLayout(animal, layout_arrivals);
+            }
+        }
+
+        for (Animal animal : SlaughterhouseTransfer) {
+            // Ajouter l'animal au layout de l'abattoir
+            addAnimalToLayout(animal, layout_slaughterhouse);
+        }
+
+        return rootView;
+    }
+
+    // Méthode pour ajouter un animal à un layout donné
+    private void addAnimalToLayout(Animal animal, LinearLayout layout) {
+        // Créer et ajouter les vues pour chaque animal
+        View animalView = LayoutInflater.from(requireContext()).inflate(R.layout.animal_item, layout, false);
+        TextView textViewNumTra = animalView.findViewById(R.id.text_view_num_tra);
+        TextView textViewNom = animalView.findViewById(R.id.text_view_nom);
+        TextView textViewDateNaiss = animalView.findViewById(R.id.text_view_date_naiss);
+        TextView textViewSexe = animalView.findViewById(R.id.text_view_sexe);
+
+        textViewNumTra.setText("Numéro de travail: " + animal.getNumTra());
+
+        String nom = animal.getNom();
+        if (nom != null) {
+            textViewNom.setText("Nom: " + animal.getNom());
+        } else {
+            Spanned spannedText = HtmlCompat.fromHtml("Nom: " + "<i>" + "non attribué" + "</i>", HtmlCompat.FROM_HTML_MODE_LEGACY);
+            textViewNom.setText(spannedText);
+        }
+
+        String dateNaissance = animal.getDateNaiss();
+        if (dateNaissance != null && dateNaissance.length() >= 10) {
+            textViewDateNaiss.setText("Date de naissance: " + dateNaissance.substring(0, 10));
+        }
+
+        if (animal.getSexe().equals("1")) {
+            textViewSexe.setText("Sexe: Mâle");
+        } else if (animal.getSexe().equals("2")) {
+            textViewSexe.setText("Sexe: Femelle");
+        }
+
+        // Ajouter la vue de l'animal au layout spécifié
+        layout.addView(animalView);
+    }
+}
---
+++ main/java/com/example/e_carterose/SelectionTransportAbattoirFragment.java
@@ -0,0 +1,114 @@
+package com.example.e_carterose;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+
+import java.util.List;
+
+
+public class SelectionTransportAbattoirFragment extends Fragment {
+
+    private DatabaseAccess db;
+    private List<Animal> allAnimals; // Variable pour stocker la liste complète des animaux
+
+    private EditText editTextNumTra;
+    private RadioGroup radioGroupTransfert;
+    private Button buttonSubmit;
+    private Button buttonEffacer;
+
+    public static SelectionTransportAbattoirFragment newInstance() {
+        SelectionTransportAbattoirFragment fragment = new SelectionTransportAbattoirFragment();
+        return fragment;
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_selection_transport_abattoir, container, false);
+
+        // Acces à la BDD
+        db = new DatabaseAccess(requireContext());
+
+        // Récupérer le numéro de l'élevage depuis la variable statique
+        String numElevage = MainActivity.numeroElevage;
+
+        // Récupérer tous les animaux de l'élevage de départ
+        allAnimals = db.getAnimalsByElevage(numElevage);
+
+        // Références des vues
+        editTextNumTra = rootView.findViewById(R.id.editTextNumTra);
+        radioGroupTransfert = rootView.findViewById(R.id.radioGroupTransfert);
+        buttonSubmit = rootView.findViewById(R.id.buttonSubmit);
+        buttonEffacer = rootView.findViewById(R.id.buttonEffacer);
+
+
+        // Bouton Soumettre
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Récupération du numéro de travail entré par l'utilisateur
+                String numTra = editTextNumTra.getText().toString();
+
+
+                // Vérifier si l'animal avec le numéro de travail donné existe dans l'élevage
+                if (!db.isNumTraExists(numTra, allAnimals)) {
+                    Toast.makeText(getContext(), "L'animal " + numTra + " n'existe pas dans votre élevage.", Toast.LENGTH_SHORT).show();
+                    return; // Arrêter l'exécution de la méthode
+                }
+
+
+                // Vérifier si un RadioButton est sélectionné dans le RadioGroup
+                if (radioGroupTransfert.getCheckedRadioButtonId() == -1) {
+                    // Aucun RadioButton n'est sélectionné
+                    Toast.makeText(requireContext(), "Veuillez sélectionner un mode de transfert.", Toast.LENGTH_SHORT).show();
+                    return; // Arrêter l'exécution de la méthode
+                }
+
+                // Si tous les champs entrés sont corrects, afficher une boîte de dialogue de confirmation
+                AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+                builder.setMessage("Êtes-vous sûr de vouloir réaliser le transfert de l'animal " + numTra + " vers l'abattoir ?");
+                builder.setPositiveButton("Oui", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Modifier le statut de l'animal (3 = transfert vers l'abattoir)
+                        db.updateStatus(numElevage, numTra, 3);
+                    }
+                });
+                builder.setNegativeButton("Non", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Ne rien faire
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
+        // Gestion du clic sur le bouton Effacer
+        buttonEffacer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Effacer les champs de texte et désélectionner les cases à cocher
+                editTextNumTra.setText("");
+                radioGroupTransfert.clearCheck();
+
+            }
+        });
+
+        return rootView;
+    }
+
+}
